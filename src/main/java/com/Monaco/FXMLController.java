a446ac153c6521778ec662a6c43cb9c9e7b9dca5
Bryce Monaco <brycedmonaco@gmail.com>
2019-09-25 23:29:15 -0700

    Main scene now uses a controller

---
+++ src/main/java/com/Monaco/FXMLController.java
@@ -0,0 +1,13 @@
+package com.Monaco;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FXMLController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
