7f0c9d78685397c423e03a5f0f2909777c2969ea
Bryce Monaco <brycedmonaco@gmail.com>
2019-10-03 21:35:17 -0700

    Added the ability to save currently listed monsters to a CSV or DM file (CSV data still)

--- src/main/java/com/Monaco/FXMLController.java
+++ src/main/java/com/Monaco/FXMLController.java
@@ -9,17 +9,25 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.stage.FileChooser;
 
 public class FXMLController implements Initializable {
+    @FXML
+    private MenuItem saveButton;
+
+    @FXML
+    private MenuItem saveAsButton;
+
     @FXML
     private ListView<Monster> monsterListView;
 
@@ -40,6 +48,8 @@ public class FXMLController implements Initializable {
 
     private ObservableList<Monster> activeMonsters;
     ObservableList<Monster> monsterList;
+
+    private String savedFilename;
 
     public FXMLController () {
         activeMonsters = FXCollections.observableArrayList();
@@ -74,7 +84,9 @@ public class FXMLController implements Initializable {
                     Monster newMonster = new Monster(monsterPickerBox.getSelectionModel().getSelectedItem());
                     Monster[] currentMonsters = activeMonsters.toArray(new Monster[1]);
                     activeMonsters.clear();
-                    activeMonsters.addAll(currentMonsters);
+                    if (currentMonsters.length > 0) {
+                        activeMonsters.addAll(currentMonsters);
+                    }
                     activeMonsters.add(newMonster);
                     System.out.println("New monster added: " + newMonster);
                     System.out.println("Monster list is now: ");
@@ -92,7 +104,68 @@ public class FXMLController implements Initializable {
             }
         });
 
+        saveButton.setOnAction(event -> {
+            if (savedFilename == null) {  // No file has been saved, treat it like a save as
+                saveMonstersAs();
+            } else {
+                File file = new File(savedFilename);
+
+                saveListedMonstersToFile(file);
+                savedFilename = file.getPath();
+
+                System.out.println("Saved a file to: " + savedFilename);
+            }
+        });
+
+        saveAsButton.setOnAction(event -> {
+            saveMonstersAs();
+        });
+
         System.out.println("Initialized!");
+
+    }
+
+    private void saveMonstersAs () {
+        if (activeMonsters.size() > 0) {
+            FileChooser fileChooser = new FileChooser();
+
+            //Set extension filter for text files
+            FileChooser.ExtensionFilter dmExtFilter = new FileChooser.ExtensionFilter("Diem files (*.dm)", "*.dm");
+            FileChooser.ExtensionFilter csvExtFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
+            fileChooser.getExtensionFilters().addAll(dmExtFilter, csvExtFilter);
+
+            //Show save file dialog
+            File file = fileChooser.showSaveDialog(((Node) monsterListView).getScene().getWindow());
+
+            if (file != null) {
+                saveListedMonstersToFile(file);
+                savedFilename = file.getPath();
+            }
+
+            System.out.println("Saved As a file to: " + savedFilename);
+        } else {
+            System.out.println("No monsters to save!");
+        }
+    }
+
+    private void saveListedMonstersToFile(File file) {
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter(file);
+
+            // Print the header line
+            writer.println("Name,Type,ALIGNMENT,Size,CR,AC,HP,Spellcasting?,Attack 1 damage,Attack 2 Damage,XP,STR,STRMod,DEX,DEXMod,CON,CONMod,INT,INTMod,WIS,WISMod,CHA,CHAMod,Page,Arctic,Coast,Desert,Forest,Grassland,Hill,Mountain,Swamp,Underdark,Underwater,Urban,Book");
+
+            for (Monster monster : activeMonsters) {
+                if (monster != null) {
+                    writer.println(monster.getCSVLine());
+                }
+            }
+
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 }
