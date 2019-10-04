6427ee4b61b20084567b6a8e87a3df52e803b352
Bryce Monaco <brycedmonaco@gmail.com>
2019-10-03 18:18:53 -0700

    Initialized fields in constructor

--- src/main/java/com/Monaco/Entities/Entity.java
+++ src/main/java/com/Monaco/Entities/Entity.java
@@ -23,6 +23,30 @@ public class Entity {
     public int cha;
     public int chaMod;
 
+    public Entity () {
+        name = "ENTITY";
+        entityClass = "ENTITYCLASS";
+        type = "ENTITYTYPE";
+        maxHP = 0;
+        currentHP= 0;
+        armorClass= 0;
+        status= 0;
+        initiative= 0;
+
+        str= 0;
+        strMod= 0;
+        dex= 0;
+        dexMod= 0;
+        con= 0;
+        conMod= 0;
+        intl= 0;
+        intlMod= 0;
+        wis= 0;
+        wisMod= 0;
+        cha= 0;
+        chaMod= 0;
+    }
+    
     @Override
     public String toString() {
         return String.format(name + " - " + entityClass);
