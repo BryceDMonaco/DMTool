dddafa42ebadd6213feb81600a6cacb41836bd36
Bryce Monaco <brycedmonaco@gmail.com>
2020-07-28 19:33:50 -0700

    Created class to contain static hex strings of colors which can be used in multiple places

---
+++ src/main/java/com/Monaco/Util/GlobalColors.java
@@ -0,0 +1,6 @@
+package com.Monaco.Util;
+
+public class GlobalColors {
+    public static final String DEAD_COLOR = "9c1d14";
+    public static final String DEFAULT_BACKGROUND_COLOR = "323232";
+}
